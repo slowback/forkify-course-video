79c7296b3ecea69a9445fb725c3a2ae9fd7a76ad
sloback <sakkara2014@hotmail.com>
2021-11-13 16:11:06 +0700

    Initial commit"

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '87e2fcda-d5ae-49ba-9cd7-02f1c9ac4254';
+export const MODAL_CLOSE_SEC = 2.5;
